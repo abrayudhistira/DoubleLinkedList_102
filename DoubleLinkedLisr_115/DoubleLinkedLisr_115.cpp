1ae5a3e663f863e08c0ad2295aed82ee7f85a9a8
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-05-25 16:00:13 +0700

    step 7

--- DoubleLinkedLisr_115/DoubleLinkedLisr_115.cpp
+++ DoubleLinkedLisr_115/DoubleLinkedLisr_115.cpp
@@ -68,4 +68,5 @@ void DoubleLinkedList::addNode() {
 	newNode->prev = current; // step 5
 	if (current->next != NULL)
 		current->next->prev = newNode; //step 6
+	current->next = newNode; //step 7
 }
