c80815241a9985da0a532ae0b80751e433ef8b5c
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-05-25 15:55:11 +0700

    1.d

--- DoubleLinkedLisr_115/DoubleLinkedLisr_115.cpp
+++ DoubleLinkedLisr_115/DoubleLinkedLisr_115.cpp
@@ -55,4 +55,7 @@ void DoubleLinkedList::addNode() {
 	Node* current = START; //step 1.a
 	Node* previous = NULL; //step 1.b
 	while (current->next != NULL && current->next->noMhs < nim)  //step 1.c
+	{
+		previous = current; //1.d
+	}
 }
